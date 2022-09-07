f1b1cce2a06229f0468bf8b27a3c0bc6fb123749
YunjungKang <99867931+Yunjung324@users.noreply.github.com>
2022-09-07 23:54:25 +0900

    Add files via upload

---
+++ 출력.py
@@ -0,0 +1,55 @@
+#501
+print('Fun Programming!')
+
+#502
+print("Programming! It's fun.")
+#502의 다른 코드
+print('Programming! It\'s fun.')
+
+#503
+print('My name is Hong Gil Dong.')
+print('I am 13 years old.')
+
+#504
+print('(@) (@)\n(=^.^=)\n(-m-m-)')
+
+#505
+print('I can program well.\nDreams come true.')
+
+#506
+print('My height\n170\nMy weight\n68.600000')
+
+#507
+print('5 Dan\n5 * 2 = 10')
+
+#508
+print("      item     count     price")
+print("       pen        20       100")
+print("      note         5        95")
+print("    eraser       110        97")
+
+#101
+print('My name is Hong')
+
+#102
+print('My hometown\nFlowering mountain')
+
+#103
+print('TTTTTTTTTT')
+print('TTTTTTTTTT')
+print('    TT    \n'*3)
+
+#104
+print('kor 90')
+print('mat 80')
+print('eng 100')
+sum = 90+80+100
+print('sum %d' %sum)
+
+#105
+print('%15s%15s%15s'%('Seoul', '10,312,545', '+91,375'))
+print('%15s%15s%15s'%('Pusan', '3,567,910', '+5,868'))
+print('%15s%15s%15s'%('Incheon','2,758,296','+64,888'))
+print('%15s%15s%15s'%('Daegu','2,511,676','+17,230'))
+print('%15s%15s%15s'%('Gwangju','1,454,636','+29,774'))
+
